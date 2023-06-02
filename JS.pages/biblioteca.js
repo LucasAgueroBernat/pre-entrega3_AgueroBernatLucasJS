051fd2edaa986018658bc40342a0e472db87c162
Lucas Aguero Bernat <lucasaguerosud@gmail.com>
2023-06-02 13:56:22 -0600

    hice modificaciones

---
+++ JS.pages/biblioteca.js
@@ -0,0 +1,74 @@
+window.addEventListener("load", function() {
+    let usuarioData = localStorage.getItem("usuarioData");
+    
+    if (usuarioData) {
+        // Convertir la cadena de texto JSON a un objeto JavaScript
+        let usuario = JSON.parse(usuarioData);
+        // Asignar el texto de bienvenida al elemento con el id "bienvenida"
+        document.getElementById("bienvenida").innerText = "¡Bienvenido/a " + usuario.nombre + " " + usuario.apellido + "!";
+    }
+  });
+
+//creamos arrays de objetos de personajes
+  const misPersonajes = [
+    {
+    nombre: "Anakin Skywalker",
+    planetaNatal: "Tatooine",
+    especie: "humano",
+    edad: "9 (ep.1) 19 (ep.2) 19-22 (clone wars) 22 (ep.3) 32 (obi-wan) 36-41 (rebels) 41(rogue one y ep.4) 44 (ep.5) 45 (ep.6)",
+    hijos: ["Luke Skywalker", "Leia Organa"],
+    padres: "Shmi Skywalker",
+    afiliacion: ["Orden Jedi (Alto Consejo Jedi)", "República Galáctica (Legión 501)", "Sith",  "Imperio Galáctico (Alto Mando Imperial y Escuadrón de la Muerte)"],
+    maestros: ["Qui-Gon Jinn", "Obi-Wan Kenobi", "Darth Sidious", "Yoda"],
+    aprendices: ["Ahsoka Tano", "Inquisición"],
+    muerte: "4 DBY Estrella de la Muerte",
+},
+{
+    nombre: "Obi-Wan Kenobi",
+    planetaNatal: "Stewjon",
+    especie: "Humano",
+    edad: "25 (ep.1), 35 (ep.2), 35-38 (clone wars), 38 (ep.3), 48 (obi-wan kenobi), 56(rebels), 57 (ep.4)",
+    afiliacion: ["Orden Jedi (Alto Consejo Jedi)", "República Galáctica (7mo Cuerpo Aéreo)","Alianza para Restaurar la República"],
+    maestros: ["Qui-Gon Jinn", "Yoda"],
+    aprendices: ["Anakin Skywalker", "Luke Skywalker"],
+    muerte: "0 ABY Estrella de la Muerte",
+},
+{
+    nombre: "Darth Sidious",
+    planetaNatal: "Naboo",
+    especie: "Humano",
+    edad: "52 (ep.1), 62 (ep.2), 62-65 (clone wars), 65 (ep.3 y the bad batch), 75 (Obi-Wan Kenobi), 79-84 (rebels), 87 (ep.5), 88 (ep.7), 119 (ep.9)",
+    afiliacion: ["Familia Palpatine", "Sith", "Casa Real de Naboo", "Rep. Galáctica (Delegaciónde Naboo, Oficina de Canciller)", "Federación de Comercio", "Confederación de Sistemas Independientes", "Imperio Galáctico", "Sith Eterno", "Primera Orden", "Orden Final"],
+    maestros: "Darth Plagueis",
+    aprendices: ["Wilhuff Tarkin", "Darth Maul", "Darth Tyranus", "Gallius Rax", "Darth Vader","Inquisición", "Snoke", "Kylo Ren"],
+    muerte: "35 DBY Exegol",
+},
+];
+//pedimos a usuario que elija su personaje 
+const personajeElejidoPorUsuario = prompt("Ingrese a quién desea conocer:\n- AnakinSkywalker\n- Obi-Wan Kenobi\n- Darth Sidious");
+//buscamos el personaje en el array y lo mostramos segun eleccion de usuario
+const personajeEncontrado = misPersonajes.find((item) => item.nombre.toLowerCase() ===
+personajeElejidoPorUsuario.toLowerCase());
+if (personajeEncontrado) {
+let mensaje = `
+    Nombre: ${personajeEncontrado.nombre}
+    Planeta Natal: ${personajeEncontrado.planetaNatal}
+    Especie: ${personajeEncontrado.especie}
+    Edad: ${personajeEncontrado.edad}
+    Hijos: ${personajeEncontrado.hijos.join(", ")}
+    Padres: ${personajeEncontrado.padres}
+    Afiliación: ${personajeEncontrado.afiliacion.join(", ")}
+    Maestros: ${personajeEncontrado.maestros.join(", ")}
+    Aprendices: ${personajeEncontrado.aprendices.join(", ")}
+    Muerte: ${personajeEncontrado.muerte}`;
+    alert(mensaje);
+} else {
+    alert("No se encontró el personaje solicitado.");
+}
+//funcion de despedida
+    function despedida() {
+    let mensaje = `Gracias ${nombre} por elegirnos para seguir aprendiendo sobre Star Wars.`;
+    alert(mensaje);
+}
+despedida();
+  
--- main.js
+++ main.js
@@ -1,67 +1,77 @@
-//Comenzamos con el juego
+// Función para aplicar los estilos del lado oscuro
+function activateDarkSide() {
+    // Cambia los estilos del body y otros elementos a los estilos del lado oscuro
+    document.body.style.backgroundColor = "#6c102b";
+    document.body.style.color = "#000";
+    document.nav.style.backgroundColor = "#6c102b";
+    document.nav.style.color = "#000";
+    // Agrega otras modificaciones de estilo necesarias para el lado oscuro
+  }
+  
+  // Función para aplicar los estilos del lado de la fuerza
+  function activateLightSide() {
+    // Restaura los estilos del body y otros elementos al lado de la fuerza
+    document.body.style.backgroundColor = "#00008b";
+    document.body.style.color = "#fff";
+    document.nav.style.backgroundColor = "#00008b";
+    document.nav.style.color = "#fff";
+    // Agrega otras modificaciones de estilo necesarias para el lado de la fuerza
+  }
+  
+  // Función para cambiar entre el lado oscuro y el lado de la fuerza
+  function toggleDarkSide() {
+    // Verifica si el modo oscuro está activado o no
+    const isDarkSide = localStorage.getItem("darkSide") === "true";
+  
+    // Si el lado oscuro está activado, desactívalo; caso contrario, actívalo
+    if (isDarkSide) {
+      localStorage.setItem("darkSide", "false");
+      activateLightSide();
+    } else {
+      localStorage.setItem("darkSide", "true");
+      activateDarkSide();
+    }
+  }
+  
+  // Evento de clic para cambiar entre el modo oscuro y el modo claro cuando se hace clic en el botón
+  document.getElementById("toggle-dark-mode").addEventListener("click", toggleDarkSide);
+  
+  // Verifica la preferencia del usuario al cargar la página y aplica los estilos correspondientes
+  const isDarkSide = localStorage.getItem("darkSide") === "true";
+  if (isDarkSide) {
+    activateDarkSide();
+  } else {
+    activateLightSide();
+  } 
 
-//Bienvenida
-let nombre = document.getElementById(nombre);
-function bienvenida() {
-let mensaje = `Bienvenido/a ${nombre}. Yo soy el Padawan Lucas Aguero y voy a enseñarte sobre algunos de nuestros personajes de STAR WARS`;
+// Función para guardar los datos del usuario en localStorage
+function saveData() {
+  let nombreDelUsuario = document.getElementById("nombre").value;
+  let apellidoDelUsuario = document.getElementById("apellido").value;
+  
+  // Crear un objeto JSON con los datos del usuario
+  let usuario = {
+    nombre: nombreDelUsuario,
+    apellido: apellidoDelUsuario,
+  };
+  
+  // Convertir el objeto JSON a una cadena de texto
+  let usuarioData = JSON.stringify(usuario);
+  
+  // Guardar los datos en localStorage
+  localStorage.setItem("usuarioData", usuarioData);
+  
+}
 
-}
-bienvenida();
-const misPersonajes = [
-    {
-    nombre: "Anakin Skywalker",
-    planetaNatal: "Tatooine",
-    especie: "humano",
-    edad: "9 (ep.1) 19 (ep.2) 19-22 (clone wars) 22 (ep.3) 32 (obi-wan) 36-41 (rebels) 41(rogue one y ep.4) 44 (ep.5) 45 (ep.6)",
-    hijos: ["Luke Skywalker", "Leia Organa"],
-    padres: "Shmi Skywalker",
-    afiliacion: ["Orden Jedi (Alto Consejo Jedi)", "República Galáctica (Legión 501)", "Sith",  "Imperio Galáctico (Alto Mando Imperial y Escuadrón de la Muerte)"],
-    maestros: ["Qui-Gon Jinn", "Obi-Wan Kenobi", "Darth Sidious", "Yoda"],
-    aprendices: ["Ahsoka Tano", "Inquisición"],
-    muerte: "4 DBY Estrella de la Muerte",
-},
-{
-nombre: "Obi-Wan Kenobi",
-planetaNatal: "Stewjon",
-especie: "Humano",
-edad: "25 (ep.1), 35 (ep.2), 35-38 (clone wars), 38 (ep.3), 48 (obi-wan kenobi), 56(rebels), 57 (ep.4)",
-afiliacion: ["Orden Jedi (Alto Consejo Jedi)", "República Galáctica (7mo Cuerpo Aéreo)","Alianza para Restaurar la República"],
-maestros: ["Qui-Gon Jinn", "Yoda"],
-aprendices: ["Anakin Skywalker", "Luke Skywalker"],
-muerte: "0 ABY Estrella de la Muerte",
-},
-{
-nombre: "Darth Sidious",
-planetaNatal: "Naboo",
-especie: "Humano",
-edad: "52 (ep.1), 62 (ep.2), 62-65 (clone wars), 65 (ep.3 y the bad batch), 75 (Obi-Wan Kenobi), 79-84 (rebels), 87 (ep.5), 88 (ep.7), 119 (ep.9)",
-afiliacion: ["Familia Palpatine", "Sith", "Casa Real de Naboo", "Rep. Galáctica (Delegaciónde Naboo, Oficina de Canciller)", "Federación de Comercio", "Confederación de Sistemas Independientes", "Imperio Galáctico", "Sith Eterno", "Primera Orden", "Orden Final"],
-maestros: "Darth Plagueis",
-aprendices: ["Wilhuff Tarkin", "Darth Maul", "Darth Tyranus", "Gallius Rax", "Darth Vader","Inquisición", "Snoke", "Kylo Ren"],
-muerte: "35 DBY Exegol",
-},
-];
-const personajeElejidoPorUsuario = prompt("Ingrese a quién desea conocer:\n- AnakinSkywalker\n- Obi-Wan Kenobi\n- Darth Sidious");
-const personajeEncontrado = misPersonajes.find((item) => item.nombre.toLowerCase() ===
-personajeElejidoPorUsuario.toLowerCase());
-if (personajeEncontrado) {
-let mensaje = `
-    Nombre: ${personajeEncontrado.nombre}
-    Planeta Natal: ${personajeEncontrado.planetaNatal}
-    Especie: ${personajeEncontrado.especie}
-    Edad: ${personajeEncontrado.edad}
-    Hijos: ${personajeEncontrado.hijos.join(", ")}
-    Padres: ${personajeEncontrado.padres}
-    Afiliación: ${personajeEncontrado.afiliacion.join(", ")}
-    Maestros: ${personajeEncontrado.maestros.join(", ")}
-    Aprendices: ${personajeEncontrado.aprendices.join(", ")}
-    Muerte: ${personajeEncontrado.muerte}`;
-    alert(mensaje);
-} else {
-    alert("No se encontró el personaje solicitado.");
-}
-    function despedida() {
-    let mensaje = `Gracias ${nombre} por elegirnos para seguir aprendiendo sobre Star Wars.`;
-    alert(mensaje);
-}
-despedida();
+
+
+// Asociar los eventos a los elementos HTML
+document.getElementById("guardar").addEventListener("click", saveData);
+
+
+
+
+//let nombre = document.getElementById(nombre);
+//function bienvenida() {
+//let mensaje = `Bienvenido/a ${nombre}. Yo soy el Padawan Lucas Aguero y voy a enseñarte sobre algunos de nuestros personajes de STAR WARS`;
+//bienvenida();
